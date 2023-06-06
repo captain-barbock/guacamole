f9c96e1c144e5e42dcdab4e65fc3c251b4acae09
Captain Barbock <captain-barbock@protonmail.ch>
2023-06-06 17:19:00 +0200

    Add migration file copy

--- lib/mix/tasks/rutli.gen.migration.ex
+++ lib/mix/tasks/rutli.gen.migration.ex
@@ -12,11 +12,12 @@ defmodule Mix.Tasks.Rutli.Gen.Migration do
     project_app = Mix.Project.config()[:app]
     IO.inspect(project_app: project_app)
 
-    ecto_repo = 
-      case Application.fetch_env(project_app, :ecto_repos) do 
+    ecto_repo =
+      case Application.fetch_env(project_app, :ecto_repos) do
         :error -> raise("There is no Ecto configuration")
         {:ok, repos} -> hd(repos)
       end
+
     IO.inspect(ecto_repo: ecto_repo)
 
     IO.inspect(app_path: Mix.Project.app_path())
@@ -25,6 +26,24 @@ defmodule Mix.Tasks.Rutli.Gen.Migration do
     rutli_path = Mix.Project.deps_paths()[:rutli]
     IO.inspect(rutli_path: rutli_path)
 
-    Mix.shell().info("Migration done")
+    migration_template =
+      Path.join([rutli_path, "priv", "templates", "rutli.gen.migration", "migration.exs"])
+
+    IO.inspect(migration_template: migration_template)
+
+    dest_path = Path.join(["priv", "migrations", timestamp() <> "_create_rutli_tables.exs"])
+
+    assigns = [ecto_repo: ecto_repo]
+
+    Mix.Generator.copy_template(migration_template, dest_path, assigns)
+
+    # Mix.shell().info("Migration done")
+  end
+
+  defp timestamp() do
+    NaiveDateTime.utc_now()
+    |> NaiveDateTime.truncate(:second)
+    |> NaiveDateTime.to_string()
+    |> String.replace(["-", " ", ":"], "")
   end
 end
